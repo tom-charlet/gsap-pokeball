0caefe6cfe32ca59e842cb5cbb4f014e4a45bbee
Tom CHARLET <charlettom11@gmail.com>
2024-07-22 21:18:30 +0200

    update

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 window.addEventListener('load', () => {
     let wrapper = document.getElementById("wrapper");
 
+    // TweenLite.set("#global-pokeball-wrapper", {
+    //     transformOrigin: "50% 100%",
+    //     yPercent: 0,
+    // });
+
     gsap.to("#pokeball-wrapper", {
-        duration: 2,
+        duration: 1.4,
         rotation: 360,
         repeat: -1,
         ease: "linear"
     })
 
-    wrapper.addEventListener('click', () => {
-        let tl = gsap.timeline();
-
-        tl.to("#global-pokeball-wrapper", {
-            yPercent: -50,
+    gsap.timeline({ repeat: -1 })
+        .to("#global-pokeball-wrapper", {
+            yPercent: -100,
             duration: 0.5,
-            ease: "circ.inOut"
+            ease: "circ.out"
         })
-
-        tl.to("#global-pokeball-wrapper", {
-            yPercent: 0,
-            duration: 0.8,
-            ease: "elastic.out"
+        .to("#global-pokeball-wrapper", {
+            yPercent: 10,
+            duration: 0.5,
+            ease: "circ.in"
         })
-
-        tl.play()
-    })
-
+        .to("#global-pokeball-wrapper", {
+            scaleY: 1.5,
+            duration: 0.4,
+            ease: "circ.in"
+        }, "<+=0.2")
+        .to("#global-pokeball-wrapper", {
+            scaleY: 0.6,
+            duration: 0.1,
+            ease: "circ.out"
+        }, "<+=0.3")
 });
